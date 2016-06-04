3f805480a746a6ce9cbe637603c828f833440916
C-Hindustries <c-h-F-15@kcs.org.uk>
2016-06-04 14:00:26 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ function start() {
   var gameStartPrompt = prompt("Welcome! Please Choose a Name For Your company!");
   companyName = gameStartPrompt;
   document.getElementById("companyName").innerHTML = companyName;
-  document.getElementById("starterDiv").className += "hidden";
-  document.getElementById("mainContentDiv").className += "mainContentDiv";
+  document.getElementById("starterDiv").className = "hidden";
+  document.getElementById("mainContentDiv").className = "mainContentDiv";
 }
