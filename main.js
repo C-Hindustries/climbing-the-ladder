cceee3b0fd695437c8d0d18b1529915ebb8176c6
C-Hindustries <c-h-F-15@kcs.org.uk>
2016-06-04 13:32:36 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-
+function start() {
+  var gameStartPrompt = prompt("Welcome! Please Choose a Name For Your company!");
+  companyName = gameStartPrompt;
+  document.getElementById("companyName").innerHTML = companyName;
+  document.getElementById("starterDiv").className += "hidden";
+  document.getElementById("mainContentDiv").className += "mainContentDiv";
+}
