8ca1f26b4101ce21fd226b254ad797f4dd725cf7
C-Hindustries <c-h-F-15@kcs.org.uk>
2016-06-05 10:52:13 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ var sales = 0
 var saleScore = 0
 var upgradeAdvertisePrice = 100
 var advertiseLevel = 0
+var upgradeLawyerPrice = 200
+var lawyerLevel = 0
+var upgradeFactoryPrice = 300
+var factoryLevel
+var upgradeOfficePrice = 300
+var officeLevel = 0
 
 function start() {
   var gameStartPrompt = prompt("Welcome! Please Choose a Name For Your company!");
@@ -13,7 +19,7 @@ function start() {
   document.getElementById("starterDiv").className = "hidden";
   document.getElementById("mainContentDiv").className = "mainContentDiv";
   document.getElementById("employmentDiv").className = "employmentDiv";
-  window.alert("the bank has given your company a loan of £500 - spend it wisely!")
+  window.alert("the bank has given your company a loan of £500 - spend it wisely! (we suggest that you upgrade all different things - it's better!)")
 }
 
 function upgradeDesign() {
@@ -30,15 +36,52 @@ function upgradeDesign() {
 
 function upgradeAdvertise() {
   if (money >= upgradeDesignPrice) {
-    money = money -= upgradeDesignPrice;
+    money = money -= upgradeAdvertisePrice;
     document.getElementById("earnNum").innerHTML = money;
     upgradeAdvertisePrice = upgradeAdvertisePrice *= 1.2;
-    document.getElementById("designerPrice").innerHTML = upgradeAdvertisePrice;
+    document.getElementById("advertisePrice").innerHTML = upgradeAdvertisePrice;
     advertiseLevel = advertiseLevel += 1;
     document.getElementById("advertiseLevel").innerHTML = advertiseLevel;
     saleScore += advertiseLevel;
   }
 }
+
+function upgradeLawyer() {
+  if (money >= upgradeLawyerPrice) {
+    money = money -= upgradeLawyerPrice;
+    document.getElementById("earnNum").innerHTML = money;
+    upgradeLawyerPrice = upgradeLawyerPrice *= 1.2;
+    document.getElementById("lawyerPrice").innerHTML = upgradeLawyerPrice;
+    lawyerLevel = lawyerLevel += 1;
+    document.getElementById("lawyerLevel").innerHTML = lawyerLevel;
+    saleScore += lawyerLevel;
+  }
+}
+
+function upgradeFactory() {
+  if (money >= upgradeFactoryPrice) {
+    money = money -= upgradeFactoryPrice;
+    document.getElementById("earnNum").innerHTML = money;
+    upgradeFactoryPrice = upgradeFactoryPrice *= 1.2;
+    document.getElementById("factoryPrice").innerHTML = upgradeFactoryPrice;
+    factoryLevel = factoryLevel += 2;
+    document.getElementById("factoryLevel").innerHTML = factoryLevel;
+    saleScore += factoryLevel;
+  }
+}
+
+function upgradeOffice() {
+  if (money >= upgradeOfficePrice) {
+    money = money -= upgOfficeradeOfficePrice;
+    document.getElementById("earnNum").innerHTML = money;
+    upgradeOfficePrice = upgradeOfficePrice *= 1.2;
+    document.getElementById("factoryPrice").innerHTML = upgradeOfficePrice;
+    officeLevel = officeLevel += 2;
+    document.getElementById("officeLevel").innerHTML = officeLevel;
+    saleScore += officeLevel;
+  }
+}
+
 
 // 1 Second Loop
 window.setInterval(function(){
