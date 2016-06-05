a5faffc7a2c4dd12a7acafb31eabca16aef03dd8
C-Hindustries <c-h-F-15@kcs.org.uk>
2016-06-05 09:38:19 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 var money = 500
+var upgradeDesignPrice = 100
+var designLevel = 0
+var sales = 0
+var saleScore = 0
 
 function start() {
   var gameStartPrompt = prompt("Welcome! Please Choose a Name For Your company!");
@@ -8,4 +12,12 @@ function start() {
   document.getElementById("mainContentDiv").className = "mainContentDiv";
   document.getElementById("employmentDiv").className = "employmentDiv";
   window.alert("the bank has given your company a loan of £500 - spend it wisely!")
+}
+
+function upgradeDesign() {
+  money = money -= upgradeDesignPrice;
+  upgradeDesignPrice = upgradeDesignPrice *= 1.2;
+  document.getElementById("designerPrice").innerHTML = upgradeDesignPrice;
+  designLevel = designLevel += 1;
+  document.getElementById("designLevel").innerHTML = designLevel;
 }
